8dd979208f05eb874fe46057559533f195bea78d
Neon Engfer <nengfer@gmail.com>
2013-08-17 16:07:29 -0700

    Get angular functioning with partials

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-console.log('got loaded!');
+'use strict'
+
+angular.module('learningApp', []).
+	config(function($routeProvider) {
+		$routeProvider.
+			when('/view1', {
+				templateUrl: '/partials/partial1.jade',
+				controller: 'Controller1'
+			}).
+			when('/view2', {
+				templateUrl: '/partials/partial2.jade',
+				controller: 'Controller2'
+			}).
+			otherwise({redirectTo: '/view1'});
+	});
---
+++ public/js/controllers.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var Controller1 = function($scope) {
+	console.log('Controller 1 loaded');
+}
+
+var Controller2 = function($scope) {
+	console.log('Controller 2 loaded');
+}
