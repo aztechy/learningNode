67552869f42e657d05070417ccdd74796cabe27f
Neon Engfer <nengfer@gmail.com>
2013-08-18 16:35:14 -0700

    add pausing functionality

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,19 +1,48 @@
 'use strict'
 
 var Controller1 = function($scope, socket) {
-	console.log('Controller 1 loaded');
+	$scope.streamToggle = 0;
+	
 	socket.on('tweet', function(data) {
-		$scope.feed.unshift(data);
+		if ($scope.streamToggle == 1) {
+			$scope.pausedFeed.unshift(data);
+		} else {
+			$scope.feed.unshift(data);
+		}
 	});
 	
 	$scope.feed = [];
+	$scope.pausedFeed = [];
 	
 	$scope.addFeedItem = function(itemName) {
 		var item = {
-			name: itemName
+			text: itemName
 		}
-		$scope.feed.unshift(item);
+		console.log($scope.streamToggle);
+		if ($scope.streamToggle == 1) {
+			console.log('save to the paused bucked');
+			$scope.pausedFeed.unshift(item);
+		} else {
+			console.log('save to the regular bucked');
+			$scope.feed.unshift(item);
+		}
+
 		$scope.feedItemName = '';
+	}
+	
+	$scope.toggleFeed = function() {
+		// When turning the feed off, copy over the buffered data.
+		if ($scope.streamToggle == 0) {
+			$scope.feed = $scope.pausedFeed.concat($scope.feed);
+			$scope.pausedFeed = [];
+			console.log('Return the container');
+		}
+	}
+	
+	$scope.updateFeed = function() {
+		console.log('Update pressed');
+		$scope.feed = $scope.pausedFeed.concat($scope.feed);
+		$scope.pausedFeed = [];
 	}
 }
 
